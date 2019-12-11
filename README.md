9d335bc8b5191199abfc87c9450cb82d1ed0aa60
simek <gosimek@gmail.com>
2019-12-11 21:42:14 +0100

    🎉 init updateMyMac.sh

---
+++ README.md
@@ -0,0 +1,19 @@
+# ⚡️ updateMyMac.sh
+
+> WARNING: Running this script may led to some bugs, issues, weird behaviour or even whole machine instability. You are using this script on your own responsibility and at your own risk.
+
+`updateMyMac.sh` is a simple Bash script which automatically updates some popular macOS libraries to the latest version available for your machine.
+
+#### Following libraries and packages will be upgraded (if installed):
+ 
+ * 💎 Ruby, RVM and gems
+ * 🍺 Brew and packages
+ * 🐍 Python, PIP and packages
+ * 📦 NPM and global packages
+ * 🐈 Yarn and global packages
+ 
+`updateMyMac.sh` will also notify you at the beginning if there are any 🍎 Software Updates available and at the end will list all installed libraries versions after update.
+
+### Tips 
+
+If your macOS machine has TouchID you can use it for authentication in Terminal prompts. To achieve that please follow the guide [on the StackOverflow](https://apple.stackexchange.com/a/306324). 
---
+++ updateMyMac.sh
@@ -0,0 +1,102 @@
+BOLD="\033[1m"
+OFF="\033[m"
+
+print_header () {
+  local PREFIX="${2:-\n\n}"
+  printf "${PREFIX}${BOLD}$1${OFF}\n"
+}
+
+print_header "⚡️ updateMyMac.sh" "\r"
+
+print_header "🔑 $(whoami) - Awaiting authentication…"
+
+print_header "🍎 Software Update"
+sudo softwareupdate -l
+
+print_header "💎 Ruby, RVM and gems"
+
+if [ -x "$(command -v rvm)" ]; then
+	rvm get stable
+	rvm install ruby --latest
+	printf "\n"
+fi
+
+if [ -x "$(command -v gem)" ]; then
+	sudo gem update --system
+	printf "\n"
+	sudo gem update
+	printf "\n"
+	sudo gem cleanup
+	printf "\n"
+	sudo gem pristine --all --only-executables
+fi
+
+if [ -x "$(command -v brew)" ]; then
+	print_header "🍺 Brew and packages"
+	brew update
+	brew upgrade
+	brew cleanup
+fi
+
+if [ -x "$(command -v pip)" ]; then
+	print_header "🐍 Python, PIP and packages"
+	pip install --upgrade pip setuptools
+	printf "\n"
+	pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+fi
+
+if [ -x "$(command -v npm)" ]; then
+	print_header "📦 NPM and global packages"
+	npm i -g npm
+	npm update -g
+fi
+
+if [ -x "$(command -v yarn)" ]; then
+	print_header "🐈 Yarn and global packages"
+	yarn global upgrade
+fi
+
+print_header "💡 Versions:" "\n"
+
+if [ -x "$(command -v python)" ]; then
+	printf "Python: "
+	python --version
+fi
+
+if [ -x "$(command -v javac)" ]; then
+	printf "Java: "
+	javac -version
+fi
+
+printf "Ruby: "
+ruby -v
+
+if [ -x "$(command -v rvm)" ]; then
+	printf "RVM: "
+	rvm -v
+fi
+
+printf "Ruby Gems: "
+gem -v
+
+if [ -x "$(command -v node)" ]; then
+	printf "Node: "
+	node -v
+fi
+
+if [ -x "$(command -v npm)" ]; then
+	printf "NPM: "
+	npm -v
+fi
+
+if [ -x "$(command -v yarn)" ]; then
+	printf "Yarn: "
+	yarn -v
+fi
+
+if [ -x "$(command -v git)" ]; then
+	printf "Git: "
+	git --version
+fi
+
+print_header "🎉 DONE!" "\n"
